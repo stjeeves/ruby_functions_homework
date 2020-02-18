9c93362854f8ccb6aed7a68c2399b0822e06a2d7
Stephen <st-jeeves@hotmail.co.uk>
2020-02-18 19:38:14 +0000

    passed cube test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,20 @@ end
 def add_string_as_number(num1, num2)
   return num1.to_i() + num2.to_i()
 end
+
+def number_to_full_month_name( month_number )
+  if month_number == "1"
+    return "January"
+  end
+end
+
+def number_to_full_month_name(month_number)
+  if month_number == 3
+    return "March"
+  end
+end
+
+
+def volume(side)
+  return side * side * side
+end
